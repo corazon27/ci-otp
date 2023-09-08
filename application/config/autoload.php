654d76d0cf26b371333a6d28ffdac75cc27f362a
corazon27 <muhalfian1899@gmail.com>
2023-09-08 13:42:15 +0700

    menambahkan sweetaler pada multiple hapus data

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'fungsi', 'security');
+$autoload['helper'] = array('url', 'file', 'fungsi', 'security', 'form');
 
 /*
 | -------------------------------------------------------------------
